edd9dca8783ad8d2ff36b6dc620d95df7e5d27c4
Wachii <dani.alvarado.sosa@gmail.com>
2026-05-25 22:46:20 +0200

    Updated to 1.0.2 (still depending on create stuff&additions 2.1.3)
    -Fixed Block picked blocks being added to sublevels in cases they shouldn't

--- src/main/java/com/wachi/sable_sa_compat/mixin/create_sa/block_picker/LiftedBlockEntityMixin.java
+++ src/main/java/com/wachi/sable_sa_compat/mixin/create_sa/block_picker/LiftedBlockEntityMixin.java
@@ -5,10 +5,14 @@ import dev.ryanhcode.sable.companion.math.BoundingBox3d;
 import dev.ryanhcode.sable.sublevel.SubLevel;
 import net.mcreator.createstuffadditions.entity.LiftedBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Set;
 
 @Mixin(LiftedBlockEntity.class)
 public class LiftedBlockEntityMixin {
@@ -18,10 +22,27 @@ public class LiftedBlockEntityMixin {
     )
     public BlockPos containing(double x, double y, double z){
         final var entity = (LiftedBlockEntity)((Object)this);
-        for (SubLevel subLevel : Sable.HELPER.getAllIntersecting(entity.level(), new BoundingBox3d(entity.getBoundingBox()))) {
-            var converted = subLevel.logicalPose().transformPositionInverse(new Vector3d(x, y, z));
-            return BlockPos.containing(converted.x(), converted.y(), converted.z());
+
+        var feetPos = entity.getOnPos();
+        if(Sable.HELPER.getContaining(
+                entity.level(),
+                feetPos
+        ) instanceof SubLevel subLevel) {
+            var bPos = BlockPos.containing(
+                    subLevel.logicalPose().transformPositionInverse(
+                            entity.getBoundingBox().getCenter()
+            ));
+            for (Direction direction : Direction.values())
+                if (!subLevel.getLevel().isEmptyBlock(bPos.relative(direction)))
+                    return bPos;
+
+            var lateral = Set.of(Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST);
+            for (Direction direction : lateral)
+                if (!subLevel.getLevel().isEmptyBlock(bPos.above().relative(direction))
+                    || !subLevel.getLevel().isEmptyBlock(bPos.below().relative(direction)))
+                    return bPos;
         }
+
         return BlockPos.containing(x, y, z);
     }
 }
